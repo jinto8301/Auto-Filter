8309e42b35a157e812ac5455a8bccf333149b5df
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2023-07-31 23:09:25 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
+git clone https://github.com/jinto8301/Auto-Filter boted;
 cd boted;
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
