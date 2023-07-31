5d74cc676f2a5434fb4703dbcb002ebfd79013ca
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2023-07-31 23:38:48 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/jinto8301/Auto-Filter /TheFileBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheFileBot
-fi
-cd /DQTheFileDonorBot
-pip3 install -U -r requirements.txt
-echo "Starting bot...."
-python3 bot.py
+rm -rf boted;
+git clone https://github.com/jinto8301/Auto-Filte /boted;
+cd boted;
+pip3 install -U -r requirements.txt;
+echo "Bot Started....";
+python3 bot.py;
