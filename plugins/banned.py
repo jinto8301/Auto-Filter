48af4b837a8342856d590570109ba64b5c1f7ba8
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2023-08-01 13:51:35 +0400

    banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -26,7 +26,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url=f'https://t.me/EnthadaNokunne')
+        InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url=f'https://t.me/jintons')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
